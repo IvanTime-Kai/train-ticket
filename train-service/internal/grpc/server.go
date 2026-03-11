320b0350310aeb247cf44e10f1163d6c197103cc
Ivantime <ivantime000@gmail.com>
2026-03-11 16:32:33 +0700

    chore: added folder proto

---
+++ train-service/internal/grpc/server.go
@@ -0,0 +1,13 @@
+package grpc
+
+// import (
+// 	db "github.com/leminhthai/train-ticket/train-service/db/generated"
+// 	proto "github.com/leminhthai/train-ticket/proto/gen/train"
+// 	"google.golang.org/grpc/codes"
+// 	"google.golang.org/grpc/status"
+// )
+
+// type TrainGRPCServer struct {
+// 	proto.UnimplementedTrainServiceServer
+// 	q *db.Queries
+// }
